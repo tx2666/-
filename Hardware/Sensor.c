6a05bd4d77ef4c01e402e1ae2b34a4661df489df
QYC <13695832800@163.com>
2025-11-25 09:27:29 +0800

    传感器获取数据

--- Hardware/Sensor.c
+++ Hardware/Sensor.c
@@ -1,5 +1,11 @@
 #include "stm32f10x.h"
 
+#define SENSOR_ID_1     ((uint8_t)1)
+#define SENSOR_ID_2     ((uint8_t)2)
+#define SENSOR_ID_3     ((uint8_t)3)
+#define SENSOR_ID_4     ((uint8_t)4)
+#define SENSOR_ID_5     ((uint8_t)5)
+
 /**
  * @brief 传感器初始化
  * @retval 无
@@ -17,4 +23,84 @@ void Sensor_Init(void)
 
     GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_15;
     GPIO_Init(GPIOB, &GPIO_InitStructure);
+}
+
+/**
+ * @brief 获取传感器1输入位
+ * @retval 0或1
+ */
+uint8_t Sensor1_GetState(void)
+{
+    uint8_t temp = GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_12);
+    return temp;
+}
+
+/**
+ * @brief 获取传感器2输入位
+ * @retval 0或1
+ */
+uint8_t Sensor2_GetState(void)
+{
+    uint8_t temp = GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_15);
+    return temp;
+}
+
+/**
+ * @brief 获取传感器3输入位
+ * @retval 0或1
+ */
+uint8_t Sensor3_GetState(void)
+{
+    uint8_t temp = GPIO_ReadInputDataBit(GPIOB, GPIO_Pin_3);
+    return temp;
+}
+
+/**
+ * @brief 获取传感器4输入位
+ * @retval 0或1
+ */
+uint8_t Sensor4_GetState(void)
+{
+    uint8_t temp = GPIO_ReadInputDataBit(GPIOB, GPIO_Pin_4);
+    return temp;
+}
+
+/**
+ * @brief 获取传感器5输入位
+ * @retval 0或1
+ */
+uint8_t Sensor5_GetState(void)
+{
+    uint8_t temp = GPIO_ReadInputDataBit(GPIOB, GPIO_Pin_5);
+    return temp;
+}
+
+/**
+ * @brief 获取指定位置的位
+ * @param Sensor_id 传感器编号，可以是SENSOR_ID_1，SENSOR_ID_2，SENSOR_ID_3，SENSOR_ID_4，SENSOR_ID_4
+ * @retval 0或1
+ */
+uint8_t Sensor_GetState(uint8_t Sensor_id)
+{
+    if (Sensor_id == SENSOR_ID_1)
+    {
+        return Sensor1_GetState();
+    }
+    else if (Sensor_id == SENSOR_ID_2)
+    {
+        return Sensor2_GetState();
+    }
+    else if (Sensor_id == SENSOR_ID_3)
+    {
+        return Sensor3_GetState();
+    }
+    else if (Sensor_id == SENSOR_ID_4)
+    {
+        return Sensor4_GetState();
+    }
+    else if (Sensor_id == SENSOR_ID_5)
+    {
+        return Sensor5_GetState();
+    }
+    return 0;
 }
--- Hardware/Sensor.h
+++ Hardware/Sensor.h
@@ -1,7 +1,14 @@
 #ifndef __SENSOR_H
 #define __SENSOR_H
-
+#include "stm32f10x.h"
 
 void Sensor_Init(void);
+uint8_t Sensor1_GetState(void);
+uint8_t Sensor2_GetState(void);
+uint8_t Sensor3_GetState(void);
+uint8_t Sensor4_GetState(void);
+uint8_t Sensor5_GetState(void);
+uint8_t Sensor_GetState(uint8_t Sensor_id);
+
 
 #endif
